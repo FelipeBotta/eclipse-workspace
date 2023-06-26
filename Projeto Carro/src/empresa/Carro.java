4e1490f429edd672185b65eed59f228dfc20acfa
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-06-26 17:05:57 -0300

    Exercicio Feito

---
+++ Projeto Carro/src/empresa/Carro.java
@@ -0,0 +1,14 @@
+package empresa;
+
+public class Carro {
+	String nome;
+	String modelo;
+	float velocidade;
+	
+	static double Pi = 3.1415; 
+	
+	
+	static float milhasParaMetros (float milhas) {
+		return milhas*1600;
+	}
+}
---
+++ Projeto Carro/src/empresa/Principal.java
@@ -0,0 +1,14 @@
+package empresa;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Carro c = new Carro();
+		
+		
+		System.out.println(Carro.Pi);
+		System.out.println(Carro.milhasParaMetros(10));
+	}
+
+}

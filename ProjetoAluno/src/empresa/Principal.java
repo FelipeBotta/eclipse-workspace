976180083ac117bacaa72b58fa8b0892718c5ba4
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-07 20:04:56 -0300

    a

--- ProjetoAluno/src/empresa/Principal.java
+++ ProjetoAluno/src/empresa/Principal.java
@@ -3,7 +3,7 @@ package empresa;
 public class Principal {
 
 	public static void main(String[] args) {
-		Aluno a1 = new Aluno("Luis",111,0.1,new Curso("Ads",1000));
+		Aluno a1 = new Aluno("Luis",111,0.1,new Curso("Analise e Desenvolvimento de Sistemas",1000));
 		
 		
 		a1.info();

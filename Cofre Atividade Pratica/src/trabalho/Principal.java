03c1695d51017a6f87f7e2e429f967567af83fd2
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-17 18:42:27 -0300

    a

--- Cofre Atividade Pratica/src/trabalho/Principal.java
+++ Cofre Atividade Pratica/src/trabalho/Principal.java
@@ -10,6 +10,8 @@ public class Principal {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		while (true){
+		
 		System.out.println("COFRINHO: \n"
 				+ "1 - Adicionar Moeda \n"
 				+ "2 - Remover Moeda \n"
@@ -67,8 +69,73 @@ public class Principal {
 			
 			
 		}
+			
+		
+		if (indice == 2){
+			
+			System.out.println("Digite a moeda: \n 1 - Euro \n 2 - Dolar \n 3 - Real");
+			
+		int sub1 = sc.nextInt();
+		
+		switch(sub1) {
+		
+		case 1:
+			
+			System.out.println("Digite o valor: ");
+			
+			double valor = sc.nextDouble();
+			
+			Euro euro = new Euro(valor);
+			
+			cofrinho.remover(euro);
+			
+			break;
+		
+		case 2:
+			
+			System.out.println("Digite o valor: ");
+			
+			double valor1 = sc.nextDouble();
+			
+			Dolar dolar = new Dolar(valor1);
+			
+			cofrinho.remover(dolar);
+			
+			break;
+			
+		case 3:
+			
+			System.out.println("Digite o valor: ");
+			
+			double valor2 = sc.nextDouble();
+			
+			Real real = new Real(valor2);
+			
+			cofrinho.remover(real);
+			
+			break;
+		
+		default:
+			
+			System.out.println("Digite uma opção valida");
 		
 		}
+		 
+		 
+		 
+		 
+		}
+		
+			
+			
+		}
+		
+		if (indice == 0){
+			break;
+		}
+		}
+		
+		
 	}
 
 }
